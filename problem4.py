5e4c29b42588b0ae959bfe56f9b732d4da25d3fd
NoahN08 <noahnbusinessinquiries@gmail.com>
2024-10-29 17:34:47 -0500

    Update problem4.py

--- problem4.py
+++ problem4.py
@@ -1,1 +1,15 @@
-
+#define the function
+def fizzBuzz(n):
+  #create a new array
+  answer = []
+  #add the separated words and numbers into the empty array, for every use case
+  for i in range(1, n + 1):
+    if i %3 == 0 and i % 5 == 0:
+      answer.append("FizzBuzz")
+    elif i % 3 == 0:
+      answer.append("Fizz")
+    elif i % 5 == 0:
+      answer.append("Buzz")
+    else:
+      answer.append(str(i))
+  return answer
