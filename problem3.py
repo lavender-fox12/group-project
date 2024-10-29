cf410e0016a9b741133e06437f4d5790357c2482
Retlewl <zyandoe@gmail.com>
2024-10-29 18:41:58 -0400

    problem3.py

--- problem3.py
+++ problem3.py
@@ -1,1 +1,9 @@
+import re
 
+def largest_number(s):
+  numbers = re.findall(r'\d+',s)
+  return max(numbers) if numbers else None
+
+string = "1,2,3,4,5"
+largest = largest_number(string)
+print(largest)
